d9ec00aa35f515a5831d67d504332c10f329578e
Wesley vieira de moura fé <wesleyvieira12@gmail.com>
2020-03-18 10:00:03 -0300

    delete

--- README.md
+++
@@ -1,2 +0,0 @@
-## Sistema de estatística de escanteios/cantos em jogos de futebol
-Link do sistema em produção: [https://wacorner.com](https://wacorner.com)
--- app/migrate-supervisor.sh
+++
@@ -1,4 +0,0 @@
-dockerize -wait tcp://redis:6379 -wait tcp://mysql:3306 -timeout 90s
-# npm install
-# composer install
-/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
